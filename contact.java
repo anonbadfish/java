ebbf423dd31971feba68bd05d491d0f721cb728c
anonbadfish <anonbadfish@users.noreply.github.com>
2014-09-18 15:55:17 -0400

    2nd contact

--- contact.java
+++ contact.java
@@ -4,5 +4,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Contact {
-	
+	public static void main(String args[]) {
+
+		ArrayList menuList = new ArrayList();
+
+			menuList.add()
+	}
 }
